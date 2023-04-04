7ae42717094d977f101a9a1cd68b455a6e69c4f7
Pradeep <55576635+pradeep71d@users.noreply.github.com>
2023-04-04 10:53:16 +0530

    Update MainActivity.java

--- app/src/main/java/com/example/mysdk4/MainActivity.java
+++ app/src/main/java/com/example/mysdk4/MainActivity.java
@@ -14,5 +14,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         //imported projrct
         //DemosssGittt
+        //take the pull from mainnnnn
     }
 }
